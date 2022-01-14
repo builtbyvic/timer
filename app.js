12a89e6b0163433d893008cb46f99bd8db02b70b
vic⚡ <builtbyvic@gmail.com>
2022-01-14 22:06:29 +0300

    refactored js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 
 // The data/time we want to countdown to
-    var countDownDate = new Date("Nov 26, 2022 08:00:00").getTime();
+    const countDownDate = new Date("Jul 10, 2022 00:00:00").getTime();
 
     // Run myfunc every second
-    var myfunc = setInterval(function() {
+    const myfunc = setInterval(function() {
 
-    var now = new Date().getTime();
-    var timeleft = countDownDate - now;
+    const now = new Date().getTime();
+    const timeleft = countDownDate - now;
         
     // Calculating the days, hours, minutes and seconds left
-    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    const days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
         
     // Result is output to the specific element
     document.getElementById("days").innerHTML = days + "d "
